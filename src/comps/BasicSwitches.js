85d41a167696c7b27a91a793d57384819a147508
analicia lara <bairdwins@gmail.com>
2021-10-23 03:36:52 -0500

    all cards work only messages left to add

--- src/comps/BasicSwitches.js
+++ src/comps/BasicSwitches.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 import Switch from "@mui/material/Switch";
+import { useEffect } from 'react';
 
 export default function BasicSwitches(props) {
   const handleChange = () => {
     props.setIsOnline(!props.isOnline);
   };
+
+  useEffect(() => {
+    console.log(props.isOnline)
+  }, [props.isOnline])
+
   return (
     <div>
       <Switch
--- src/comps/Slider.js
+++ src/comps/Slider.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
+import { useEffect } from "react";
 
 export default function DiscreteSlider(props) {
   const handleChange = (e) => {
     props.setVolume(e.target.value);
-    console.log(props.volume);
   };
+
+  useEffect(() => {
+    console.log(props.volume)
+  }, [props.volume])
 
   return (
     <Box sx={{ width: 300 }}>
